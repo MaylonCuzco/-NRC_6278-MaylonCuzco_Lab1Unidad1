21b1040f40f2d564b8053d2c23e9e7e9054d2245
MaylonCuzco <mecuzco@espe.edu.ec>
2022-05-25 23:09:41 -0500

    Mediante la sentencia elif podemos agregar varias condiciones, mediante esto realizamos el la misma estructura de la Parcela 1 pero en este caso el robot se ubica en la Parcela 2 a esta ubicacion la definimos como 1 en la variable "robotubicacion".

--- robotAgricultor.py
+++ robotAgricultor.py
@@ -75,3 +75,64 @@ class EntornoRobot(Parcelas):
                       print("5. NO hay mala hierba en la Parcela 3")
                       print("------ finaliza -------")
                       print("costo:", costo)
+
+######################################################################################################################
+
+        #El robot agricultor comienza por la Parcela 2
+    elif robotubicacion == 1:
+            print("1. El robot agricultor se ubica en la Parcela 2")
+            
+            if parcela.ubicacionestado['P2'] == 1:
+                print("2. La Parcela 2 está con mala hierba")
+                parcela.ubicacionestado['P2'] == 0
+                costo = costo + 1
+                print("3. Se eliminó la mala hierba de la Parcela 2, el robot se mueve a la Parcela 3")
+
+                if parcela.ubicacionestado['P3'] == 1:
+                    print("4. La Parcela 3 está con mala hierba")
+                    parcela.ubicacionestado['P3'] == 0
+                    costo = costo + 1
+                    print("5. Se eliminó la mala hierba de la Parcela 3, el robot se mueve a la Parcela 1")
+                    
+                else:
+                   print("4. NO hay mala hierba en la Parcela 3, el robot de mueve a la Parcela 1")
+
+                if parcela.ubicacionestado['P1'] == 1:
+                         print("6. La Parcela 1 está con mala hierba")
+                         parcela.ubicacionestado['P1'] == 0
+                         costo = costo + 1
+                         print("7. Se eliminó la mala hierba de la Parcela 1")
+                         print("------finalizar------")
+                         print("costo:", costo)
+                else:
+                     print("6. NO hay mala hierba en la Parcela 1")
+                     print("------finalizar------")
+                     print("costo:", costo)  
+            
+            # Si la parcela 2 esta sin mala hierba, el robot se mueve hacia la Parcela 3 y despues a la Parcela 1
+            else:
+               
+                print("2. NO hay mala hierba en la Parcela 2, el robot se mueve a la Parcela 3")
+               
+                if parcela.ubicacionestado['P3'] == 1:
+                    print("3. La Parcela 3 está con mala hierba")
+                    parcela.ubicacionestado['P3'] == 0
+                    costo = costo + 1
+                    print("4. Se eliminó la mala hierba de la Parcela 3, el robot se mueve a la Parcela 1")
+                else:
+                    print("3. NO hay mala hierba en la Parcela 3, el robot se mueve a la Parcela 1")
+
+                if parcela.ubicacionestado['P1'] == 1:
+                     print("5. La Parcela 1 está con mala hierba")
+                     parcela.ubicacionestado['P1'] == 0
+                     costo = costo + 1
+                     print("6. Se eliminó la mala hierba de la Parcela 1")
+                     print("------finalizar------")
+                     print("costo:", costo)
+
+                else:
+                      print("5. NO hay mala hierba en la Parcela 1")
+                      print("------finalizar------")
+                      print("costo:", costo)
+
+######################################################################################################################
