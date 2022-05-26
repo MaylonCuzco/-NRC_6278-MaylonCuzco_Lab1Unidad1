5a195b9658fe6c403f36953bf4f23c4480ec26ae
MaylonCuzco <mecuzco@espe.edu.ec>
2022-05-25 23:00:42 -0500

    Se crea una clase que hace referencia al entono del robor, en el cual se definen los estados y las ubicacion en la que se encuentra el robot

--- robotAgricultor.py
+++ robotAgricultor.py
@@ -3,8 +3,19 @@
 #UBICACIONES
 #Parcela 1 = P1, Parcela 2 = P2, Parcela 3 = P3
 import random
-class parcela(object):
+class Parcelas(object):
   def __init__(self):
 #crear instancias entre ubicaciones y intrucciones aleatoria entre el 0 y 2
-    self.locationcondition =  {'A': random.randint(0,2), 'B': random.randint(0,2), 'C': random.randint(0,2)}
-    
+   self.ubicacionestado =  {'P1': random.randint(0,1), 'P2': random.randint(0,1), 'P3': random.randint(0,1)}
+        
+
+class EntornoRobot(Parcelas):
+  def __init__(self, parcela):
+    print("---------- ROBOT AGRICULTOR -----------")
+    print("Estado: ")
+    print(parcela.ubicacionestado)
+    print("Estado objetivo: {[P1:0],[P2:0],[P3:0]}")
+    # El robot esta en una ubicación aleatoria
+    robotubicacion = random.randint(0,2)
+    costo=0
+
